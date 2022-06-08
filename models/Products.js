73600b78de276858fdf061de35eb1617c567b6c9
mihaicc30 <77832895+mihaicc30@users.noreply.github.com>
2022-06-08 17:38:44 +0100

    1

---
+++ models/Products.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const ProductsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: String,
+    required: true
+  },
+  img: {
+    type: String,
+    default: "null"
+  },
+  cat: {
+    type: String,
+    default: "null"
+  }
+});
+
+const Products = mongoose.model('Products', ProductsSchema);
+
+module.exports = Products;
